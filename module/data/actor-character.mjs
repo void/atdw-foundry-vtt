c0760481b7a80901434b92bb0cecede3f5db21e7
Ismael Carnales <icarnales@gmail.com>
2024-08-24 23:52:39 -0300

    remove ability modifiers

--- module/data/actor-character.mjs
+++ module/data/actor-character.mjs
@@ -27,8 +27,6 @@ export default class ATDWCharacter extends ATDWActorBase {
   prepareDerivedData() {
     // Loop through ability scores, and add their modifiers to our sheet output.
     for (const key in this.abilities) {
-      // Calculate the modifier using d20 rules.
-      this.abilities[key].mod = Math.floor((this.abilities[key].value - 10) / 2);
       // Handle ability label localization.
       this.abilities[key].label = game.i18n.localize(CONFIG.ATDW.abilities[key]) ?? key;
     }
