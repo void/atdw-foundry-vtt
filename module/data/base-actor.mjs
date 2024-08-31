f66e9b2ae3390f0bbe5d7b517d75df31409561cb
Ismael Carnales <icarnales@gmail.com>
2024-08-31 12:34:26 -0300

    remove power and biography from base actor

--- module/data/base-actor.mjs
+++ module/data/base-actor.mjs
@@ -11,11 +11,6 @@ export default class ATDWActorBase extends ATDWDataModel {
       value: new fields.NumberField({ ...requiredInteger, initial: 3, min: 0 }),
       max: new fields.NumberField({ ...requiredInteger, initial: 3 })
     });
-    schema.power = new fields.SchemaField({
-      value: new fields.NumberField({ ...requiredInteger, initial: 5, min: 0 }),
-      max: new fields.NumberField({ ...requiredInteger, initial: 5 })
-    });
-    schema.biography = new fields.StringField({ required: true, blank: true }); // equivalent to passing ({initial: ""}) for StringFields
 
     return schema;
   }
